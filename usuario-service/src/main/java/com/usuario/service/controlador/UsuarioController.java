bd0147a854ca55efeea4daaeddbf5fd3e18166bc
Edwar Paredes <eparedes.se@gmail.com>
2023-01-27 18:49:43 -0500

    Add microservices

---
+++ usuario-service/src/main/java/com/usuario/service/controlador/UsuarioController.java
@@ -0,0 +1,110 @@
+package com.usuario.service.controlador;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.usuario.service.entidades.Usuario;
+import com.usuario.service.modelos.Carro;
+import com.usuario.service.modelos.Moto;
+import com.usuario.service.servicio.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+	private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> listarUsuarios(){
+		List<Usuario> usuarios = usuarioService.getAll();
+		if(usuarios.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(usuarios);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") int id){
+		Usuario usuario = usuarioService.getUsuarioById(id);
+		if(usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(usuario);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario){
+		Usuario nuevoUsuario = usuarioService.save(usuario);
+		return ResponseEntity.ok(nuevoUsuario);
+	}
+	
+	@GetMapping("/carros/{usuarioId}")
+	//@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource was not found on the server")
+	public ResponseEntity<List<Carro>> listarCarros(@PathVariable("usuarioId") int id){
+		try {
+			Usuario usuario = usuarioService.getUsuarioById(id);
+			if(usuario == null) {
+				log.info("No existe Usuario");
+				return ResponseEntity.notFound().build();
+				//throw new RuntimeException("No existe Usuario");
+			}
+			log.info("Llamando getCarros");
+			List<Carro> carros = usuarioService.getCarros(id);
+			return ResponseEntity.ok(carros);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+            //throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+			throw new RuntimeException(e.getMessage());
+		}	
+	}
+	
+	@GetMapping("/motos/{usuarioId}")    
+	public ResponseEntity<List<Moto>> listarMotos(@PathVariable("usuarioId") int id){
+		try {
+			Usuario usuario = usuarioService.getUsuarioById(id);
+			if(usuario == null) {
+				return ResponseEntity.notFound().build();
+			}
+			log.info("Llamando getMotos");
+			List<Moto> motos = usuarioService.getMotos(id);
+			return ResponseEntity.ok(motos);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
+		}			
+	}
+	
+	@PostMapping("/carro/{usuarioId}")
+	public ResponseEntity<Carro> guardarCarro(@PathVariable("usuarioId") int usuarioId,@RequestBody Carro carro){
+		Carro nuevoCarro = usuarioService.saveCarro(usuarioId, carro);
+		return ResponseEntity.ok(nuevoCarro);
+	} 
+	
+	@PostMapping("/moto/{usuarioId}")
+	public ResponseEntity<Moto> guardarMoto(@PathVariable("usuarioId") int usuarioId,@RequestBody Moto moto){
+		Moto nuevaMoto = usuarioService.saveMoto(usuarioId, moto);
+		return ResponseEntity.ok(nuevaMoto);
+	}
+	
+	@GetMapping("/todos/{usuarioId}")
+	public ResponseEntity<Map<String, Object>> listarTodosLosVehiculos(@PathVariable("usuarioId") int usuarioId){
+		Map<String,Object> resultado = usuarioService.getUsuarioAndVehiculos(usuarioId);
+		return ResponseEntity.ok(resultado);
+	}
+}
